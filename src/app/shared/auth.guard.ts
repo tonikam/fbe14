93ba36d3d26f80b3ca80005480e15b1a47961822
Dev2 <Dev2>
2016-09-25 23:30:05 +0200

    comment in auth.guard.ts

--- src/app/shared/auth.guard.ts
+++ src/app/shared/auth.guard.ts
@@ -8,6 +8,12 @@ import { AuthService } from "./auth.service";
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService) {}
 
+  // aktuell nicht genutzt:
+  // canActivate: Parameter bei den Routes ( siehe app.routing.ts )
+  // example: {path: 'diseaseEvent', component: DiseaseEventComponent, canActivate: [AuthGuard]},
+  //
+  // momentan im html über [ *ngIf="af.auth | async" ] gelöst
+
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean {
     return this.authService.isAuthenticated();
   }
